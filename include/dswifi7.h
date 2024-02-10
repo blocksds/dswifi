2c8dea303b8cd0dd388da8cf03cb67976b27a67d
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-02-10 12:16:26 +0000

    docs: Reformat documentation to use Doxygen

--- include/dswifi7.h
+++ include/dswifi7.h
@@ -2,7 +2,11 @@
 //
 // Copyright (C) 2005-2006 Stephen Stair - sgstair@akkit.org - http://www.akkit.org
 
-// DSWifi Project - Arm7 Library Header file (dswifi7.h)
+// DSWifi Project - ARM7 library header file (dswifi7.h)
+
+/// @file dswifi7.h
+///
+/// @brief ARM7 header of DSWifi.
 
 #ifndef DSWIFI7_H
 #define DSWIFI7_H
@@ -13,53 +17,69 @@ extern "C" {
 
 #include "dswifi_version.h"
 
-// Wifi Sync Handler function: Callback function that is called when the arm9 needs to be told to synchronize with new fifo data.
-// If this callback is used (see Wifi_SetSyncHandler()), it should send a message via the fifo to the arm9, which will call Wifi_Sync() on arm9.
+/// Wifi Sync Handler function.
+///
+/// Callback function that is called when the arm9 needs to be told to
+/// synchronize with new fifo data. If this callback is used (see
+/// Wifi_SetSyncHandler()), it should send a message via the fifo to the ARM9,
+/// which will call Wifi_Sync() on ARM9.
 typedef void (*WifiSyncHandler)();
 
-// Read_Flash: Reads an arbitrary amount of data from the firmware flash chip
-//  int address:        Offset to start reading from in the flash chip
-//  char * destination: Pointer to a memory buffer to hold incoming data
-//  int length:         The number of bytes to read
+/// Reads an arbitrary amount of data from the firmware flash chip.
+///
+/// @param address     Offset to start reading from in the flash chip.
+/// @param destination Pointer to a memory buffer to hold incoming data.
+/// @param length      The number of bytes to read.
 extern void Read_Flash(int address, char * destination, int length);
 
-// PowerChip_ReadWrite: Reads or writes a value to the DS's power chip
-//  int cmp:	The byte-long command to send to the chip (top bit 1=read, 0=write - other bits = register ID to read/write)
-//  int data:	The data to write to the chip (if sending a read command, this should be zero)
-//  Returns:	The data read returned by the serial connection; only really useful when reading.
+/// Reads or writes a value to the DS's power chip.
+///
+/// @param cmd  The byte-long command to send to the chip (top bit 1=read,
+///             0=write - other bits = register ID to read/write)
+/// @param data The data to write to the chip (if sending a read command,
+///             this should be zero)
+/// @return     The data read returned by the serial connection; only really
+///             useful when reading.
 extern int PowerChip_ReadWrite(int cmd, int data);
 
-// Wifi_Interrupt: Handler for the arm7 wifi interrupt. Should be called by the
-//   interrupt handler on arm7, and should *not* have multiple interrupts enabled.
+/// Handler for the ARM7 WiFi interrupt.
+///
+/// It should be called by the interrupt handler on ARM7, and should *not* have
+/// multiple interrupts enabled.
 extern void Wifi_Interrupt();
 
-// Wifi_Update: Sync function to ensure data continues to flow between the two
-//   CPUs smoothly.  Should be called at a periodic interval, such as in vblank.
+/// Sync function to ensure data continues to flow between the two CPUs smoothly.
+///
+/// It Should be called at a periodic interval, such as in VBlank.
 extern void Wifi_Update();
 
-// Wifi_Init: Requires the data returned by the arm9 wifi init call. The arm9
-//   init call's returned data must be passed to the arm7 and then given to this
-//   function. (or else very bad things happen)
-//   This function also enables power to the wifi system, which will shorten
-//   battery life.
-//  unsigned long WifiData: You must pass the 32bit value returned by the call to
-//                           Wifi_Init on the ARM9.
+/// Requires the data returned by the ARM9 WiFi init call.
+///
+/// The data returned by the ARM9 init call must be passed to the ARM7 and then
+/// given to this function.
+///
+/// This function also enables power to the WiFi system, which will shorten
+/// battery life.
+///
+/// @param WifiData You must pass the 32bit value returned by the call to
+///                 Wifi_Init on the ARM9.
 extern void Wifi_Init(unsigned long WifiData);
 
-// Wifi_Deinit: In the case that it is necessary, this function cuts power to
-//   the wifi system.  After this the wifi will be unusable until Wifi_Init is
-//   called again.
+/// In case it is necessary, this function cuts power to the WiFi system.
+///
+/// After this WiFi will be unusable until Wifi_Init() is called again.
 extern void Wifi_Deinit();
 
-// Wifi_Sync: Call this function when requested to sync by the arm9 side of the 
-//   wifi lib
+/// Call this function when requested to sync by the ARM9 side of the WiFi lib.
 extern void Wifi_Sync();
 
-// Wifi_SetSyncHandler: Call this function to request notification of when the
-//   ARM9-side Wifi_Sync function should be called.
-//  WifiSyncHandler sh:    Pointer to the function to be called for notification.
+/// Call this function to request notification of when the ARM9-side Wifi_Sync()
+/// function should be called.
+///
+/// @param sh Pointer to the function to be called for notification.
 extern void Wifi_SetSyncHandler(WifiSyncHandler sh);
 
+/// Setup system FIFO handler for WiFi library messages.
 extern void installWifiFIFO();
 
 #ifdef __cplusplus
