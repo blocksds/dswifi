4cf906bee8971bc474b17af346bf3e4dd724ea99
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-01-19 02:59:23 +0000

    chore: Add definitions for RF9008
    Thanks to asie for the original definitions, created from the documentation
    in GBATEK.

--- source/arm7/wifi_rf.h
+++ source/arm7/wifi_rf.h
@@ -14,6 +14,32 @@
 extern "C" {
 #endif
 
+// Hardware definitions for the RF9008 transceiver chip.
+
+#define REG_RF9008_CFG1     0x00
+#define REG_RF9008_IFPLL1   0x01
+#define REG_RF9008_IFPLL2   0x02
+#define REG_RF9008_IFPLL3   0x03
+#define REG_RF9008_RFPLL1   0x04
+#define REG_RF9008_RFPLL2   0x05
+#define REG_RF9008_RFPLL3   0x06
+#define REG_RF9008_RFPLL4   0x07
+#define REG_RF9008_CAL1     0x08
+
+#define REG_RF9008_TXRX1    0x09
+
+#define RF9008_TXRX_PCONTROL_EXTERNAL       (0)
+#define RF9008_TXRX_PCONTROL_INTERNAL_TXVGC (2 << 15)
+#define RF9008_TXRX_PCONTROL_INTERNAL_POWER (3 << 15)
+#define RF9008_TXRX_PCONTROL_MASK           (3 << 15)
+#define RF9008_TXRX_TXVGC_SHIFT             9
+#define RF9008_TXRX_TXVGC_MASK              (31 << 9)
+
+#define REG_RF9008_PCNT1    0x0A
+#define REG_RF9008_PCNT2    0x0B
+#define REG_RF9008_VCOT1    0x0C
+#define REG_RF9008_RESET    0x1F
+
 void Wifi_RFWrite(int writedata);
 void Wifi_RFInit(void);
 
