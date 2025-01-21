d13822f842c95b37848c9210b3c31b5101de227f
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-01-21 01:49:00 +0000

    chore: Extract ARM9 heap functions to their own file

---
+++ source/arm9/heap.h
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (C) 2005-2006 Stephen Stair - sgstair@akkit.org - http://www.akkit.org
+
+#ifndef WIFI_ARM9_IPC_H__
+#define WIFI_ARM9_IPC_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef WIFI_USE_TCP_SGIP
+
+void wHeapAllocInit(int size);
+void *wHeapAlloc(int size);
+void wHeapFree(void *data);
+
+#endif // WIFI_USE_TCP_SGIP
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif // WIFI_ARM9_IPC_H__
