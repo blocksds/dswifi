blocksds/dswifi

commit 6edc0df1686beaf12beaac25f8a05900a6720292

ipc: Let users change channel again
Only prevent manual changes while in scan mode (because the channel changes
periodically anyway).

source/arm7/update.c
@@ -334,9 +334,14 @@ void Wifi_Update(void)
             break;
     }
 
-    if (WifiData->curChannel != WifiData->reqChannel)
+    // Only allow manual changes of the channel if scan mode isn't active
+    // because scan mode changes the channel periodically anyway.
+    if (WifiData->curMode != WIFIMODE_SCAN)
     {
-        Wifi_SetChannel(WifiData->reqChannel);
+        if (WifiData->curChannel != WifiData->reqChannel)
+        {
+            Wifi_SetChannel(WifiData->reqChannel);
+        }
     }
 
     if (WifiData->reqReqFlags & WFLAG_REQ_STOPBEACON)

source/arm9/ipc.c
@@ -249,6 +249,5 @@ void Wifi_SetChannel(int channel)
     if (channel < 1 || channel > 13)
         return;
 
-    if (WifiData->reqMode == WIFIMODE_NORMAL)
-        WifiData->reqChannel = channel;
+    WifiData->reqChannel = channel;
 }

commit 3591f85940c2332829790084c2e87b2cac654294

scan: Fix channel change during scan mode
Commit 6edc0df1686b ("ipc: Let users change channel again") broke scan
mode. The ARM9 sets reqChannel and the ARM7 reads that to update the
channel. However, the ARM7 also uses that variable to iterate through
all channels. Instead of that, the ARM7 now sets the channel directly
from the scan mode code, but it also sets reqChannel so that after scan
mode the last channel remains.

source/arm7/update.c
@@ -235,6 +235,7 @@ void Wifi_Update(void)
                 // Request changing channel
                 WifiData->counter7   = W_US_COUNT1;
                 WifiData->reqChannel = scanlist[wifi_scan_index];
+                Wifi_SetChannel(WifiData->reqChannel);
 
                 // Update timeout counters of all APs.
                 for (int i = 0; i < WIFI_MAX_AP; i++)