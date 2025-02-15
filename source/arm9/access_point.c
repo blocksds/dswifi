blocksds/dswifi

commit af3cc4a5c41dae9372c202192e53cb794029c0a6

ap: Check arguments correctly

source/arm9/access_point.c
@@ -36,7 +36,7 @@ int Wifi_GetNumAP(void)
 
 int Wifi_GetAPData(int apnum, Wifi_AccessPoint *apdata)
 {
-    if (!apdata)
+    if ((apnum <= 0) || (apdata == NULL))
         return WIFI_RETURN_PARAMERROR;
 
     if (WifiData->aplist[apnum].flags & WFLAG_APDATA_ACTIVE)

commit 747347a4aafbdb0afa92ae140a65e6c2a5af523b

ap: Fix argument check
Commit af3cc4a5c41d ("ap: Check arguments correctly") broke
Wifi_GetAPData() when AP index 0 was requested.

source/arm9/access_point.c
@@ -36,7 +36,7 @@ int Wifi_GetNumAP(void)
 
 int Wifi_GetAPData(int apnum, Wifi_AccessPoint *apdata)
 {
-    if ((apnum <= 0) || (apdata == NULL))
+    if ((apnum < 0) || (apdata == NULL))
         return WIFI_RETURN_PARAMERROR;
 
     if (WifiData->aplist[apnum].flags & WFLAG_APDATA_ACTIVE)