4fa6898bb311b80ba1cb8c86882f9cb5433f8986
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-02-23 21:54:31 +0000

    docs: Move main Doxygen page to a markdown file
    This will make it easier to improve it later.

---
+++ documentation/main_page.md
@@ -0,0 +1,21 @@
+DSWifi documentation {#mainpage}
+====================
+
+## Introduction
+
+A C library to use the WiFi hardware of the Nintendo DS.
+
+## API documentation
+
+- ARM9
+  - @ref dswifi9_general "General state of the library."
+  - @ref dswifi9_ap "Scan and connect to access points."
+  - @ref dswifi9_mp_host "Local multiplayer host utilities."
+  - @ref dswifi9_ip "Utilities related to Internet access."
+  - @ref dswifi9_raw_tx_rx "Raw transfer/reception of packets."
+
+- ARM7
+  - @ref dswifi7.h "ARM7 DSWifi header"
+
+- Common
+  - @ref dswifi_common.h "ARM9 and ARM7 common definitions"
--- include/dswifi9.h
+++ include/dswifi9.h
@@ -6,27 +6,6 @@
 /// @file dswifi9.h
 ///
 /// @brief ARM9 header of DSWifi.
-///
-/// @mainpage DSWifi documentation
-///
-/// ## Introduction
-///
-/// A C library to use the WiFi hardware of the Nintendo DS.
-///
-/// ## API documentation
-///
-/// - ARM9
-///   - @ref dswifi9_general "General state of the library."
-///   - @ref dswifi9_ap "Scan and connect to access points."
-///   - @ref dswifi9_mp_host "Local multiplayer host utilities."
-///   - @ref dswifi9_ip "Utilities related to Internet access."
-///   - @ref dswifi9_raw_tx_rx "Raw transfer/reception of packets."
-///
-/// - ARM7
-///   - @ref dswifi7.h "ARM7 DSWifi header"
-///
-/// - Common
-///   - @ref dswifi_common.h "ARM9 and ARM7 common definitions"
 
 #ifndef DSWIFI9_H
 #define DSWIFI9_H
