dcea996f505bb05544b2b5d81e171436a02a1b49
Antonio Niño Díaz <antonio_nd@proton.me>
2026-01-27 22:45:08 +0000

    twl: Use new NDMA register definitions

--- source/arm7/twl/ndma.twl.c
+++ source/arm7/twl/ndma.twl.c
@@ -8,9 +8,9 @@
 
 void wifi_ndma_init(void)
 {
-    NDMA_CR(WIFI_NDMA_CHAN) = 0;
+    REG_NDMA_CR(WIFI_NDMA_CHAN) = 0;
 
-    NDMA_GCR = NDMA_GCR_FIXED_METHOD;
+    REG_NDMA_GCR = NDMA_GCR_FIXED_METHOD;
 }
 
 void wifi_ndma_wait(void)
@@ -20,38 +20,38 @@ void wifi_ndma_wait(void)
 
 void wifi_ndma_read(void *dst, u32 len)
 {
-    NDMA_SRC(WIFI_NDMA_CHAN) = (u32)(REG_SDIO_BASE + WIFI_SDIO_OFFS_DATA32_FIFO);
-    NDMA_DEST(WIFI_NDMA_CHAN) = (u32)dst;
-    NDMA_LENGTH(WIFI_NDMA_CHAN) = len / 4;
+    REG_NDMA_SRC(WIFI_NDMA_CHAN) = (u32)(REG_SDIO_BASE + WIFI_SDIO_OFFS_DATA32_FIFO);
+    REG_NDMA_DEST(WIFI_NDMA_CHAN) = (u32)dst;
+    REG_NDMA_LENGTH(WIFI_NDMA_CHAN) = len / 4;
 
     // Logical block size, set to SDIO block len
-    NDMA_BLENGTH(WIFI_NDMA_CHAN) = 0x80 / 4;
+    REG_NDMA_BLENGTH(WIFI_NDMA_CHAN) = 0x80 / 4;
 
     // Physical block size is also set to 0x80 bytes. There's probably a better
     // value depending on what memory we're transferring to.
-    NDMA_CR(WIFI_NDMA_CHAN) = NDMA_ENABLE
-                            | NDMA_BLOCK_SCALER(0x80 / 4)
-                            | NDMA_START_TWL_WIFI
-                            | NDMA_SRC_FIX
-                            | NDMA_DST_INC;
+    REG_NDMA_CR(WIFI_NDMA_CHAN) = NDMA_ENABLE
+                                | NDMA_BLOCK_SCALER(0x80 / 4)
+                                | NDMA_START_TWL_WIFI
+                                | NDMA_SRC_FIX
+                                | NDMA_DST_INC;
 }
 
 void wifi_ndma_write(const void *src, u32 len)
 {
     wifi_ndma_wait();
 
-    NDMA_SRC(WIFI_NDMA_CHAN) = (u32)src;
-    NDMA_DEST(WIFI_NDMA_CHAN) = (u32)(REG_SDIO_BASE + WIFI_SDIO_OFFS_DATA32_FIFO);
-    NDMA_LENGTH(WIFI_NDMA_CHAN) = len / 4;
+    REG_NDMA_SRC(WIFI_NDMA_CHAN) = (u32)src;
+    REG_NDMA_DEST(WIFI_NDMA_CHAN) = (u32)(REG_SDIO_BASE + WIFI_SDIO_OFFS_DATA32_FIFO);
+    REG_NDMA_LENGTH(WIFI_NDMA_CHAN) = len / 4;
 
     // Logical block size, set to SDIO block len
-    NDMA_BLENGTH(WIFI_NDMA_CHAN) = 0x80 / 4;
+    REG_NDMA_BLENGTH(WIFI_NDMA_CHAN) = 0x80 / 4;
 
     // Physical block size is also set to 0x80 bytes. There's probably a better
     // value depending on what memory we're transferring to.
-    NDMA_CR(WIFI_NDMA_CHAN) = NDMA_ENABLE
-                            | NDMA_BLOCK_SCALER(0x80 / 4)
-                            | NDMA_START_TWL_WIFI
-                            | NDMA_SRC_INC
-                            | NDMA_DST_FIX;
+    REG_NDMA_CR(WIFI_NDMA_CHAN) = NDMA_ENABLE
+                                | NDMA_BLOCK_SCALER(0x80 / 4)
+                                | NDMA_START_TWL_WIFI
+                                | NDMA_SRC_INC
+                                | NDMA_DST_FIX;
 }
